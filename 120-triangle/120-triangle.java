fb18474df1f66da589e0c0a6cf5ef1d10722f84c
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-05-17 19:10:27 -0400

    Time: 10 ms (17.47%), Space: 44.5 MB (22.31%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,46 +1,23 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int result = Integer.MAX_VALUE;
+        List<Integer> prev = new ArrayList<>();
+        prev.add(triangle.get(0).get(0));
         
-        if(triangle.size() == 1)
+        for(int i = 1; i < triangle.size(); i++)
         {
-            for(int i = 0; i < triangle.get(0).size(); i ++){
-                result = Math.min(result, triangle.get(0).get(i));;
-            }
-            return result;
-        }
-        List<List<Integer>> dp = new ArrayList<>();
-        
-        for(List<Integer> t : triangle)
-        {
-            dp.add(new ArrayList<>(t));
-        }
-        
-        for(int i = 1; i < dp.size(); i++)
-        {
-            for(int j = 0; j < dp.get(i).size(); j++)
-            {
-                dp.get(i).set(j, Math.min((j - 1 >= 0 ? dp.get(i - 1).get(j - 1) : Integer.MAX_VALUE), (j < dp.get(i - 1).size() ? dp.get(i - 1).get(j) : Integer.MAX_VALUE)) + triangle.get(i).get(j));
-                
-                if(i == dp.size() - 1)
-                {
-                    result = Math.min(result, dp.get(i).get(j));
-                }
-            }
-        }
-        
-        
-        // for(int i = 1; i < dp.size(); i++)
-        // {
-        //     for(int j = 0; j < dp.get(i).size(); j++)
-        //     {
-        //         System.out.print(dp.get(i).get(j) + " ");
-        //     }
-        //     System.out.println();
-        // }
             
+            List<Integer> curt = new ArrayList<>();
+            
+            for(int j = 0; j < triangle.get(i).size(); j++)
+            {
+                curt.add(Math.min((j - 1 >= 0 ? prev.get(j - 1) : Integer.MAX_VALUE), (j < prev.size() ? prev.get(j) : Integer.MAX_VALUE)) + triangle.get(i).get(j));
                 
+            }
+            
+            prev = curt;
+        }
         
-        return result;
+        return Collections.min(prev);
     }
 }
