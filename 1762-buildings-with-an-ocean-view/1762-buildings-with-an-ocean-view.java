fc2795f9f91c7b910c275ad732c2ce4d561b96ba
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-05-17 13:08:56 -0400

    Time: 46 ms (24.40%), Space: 82.8 MB (67.97%) - LeetHub

---
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] findBuildings(int[] heights) {
+        int N = heights.length;
+        
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        
+        
+        for(int i = N - 1; i > -1; i--)
+        {
+            if(!stack.isEmpty())
+            {
+                if(heights[stack.peek()] < heights[i])
+                {
+                    stack.push(i);
+                }
+            }
+            else
+            {
+                stack.push(i);
+            }
+        }
+        
+        int[] result = new int[stack.size()];
+        int p = 0;
+        
+        while(!stack.isEmpty())
+        {
+            result[p++] = stack.pop();
+        }
+        
+        return result;
+    }
+}
