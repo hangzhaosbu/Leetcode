723a97766fce43b2c08ed9eb43e3d174b718d19b
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-05-20 20:58:25 -0400

    Time: 50 ms (22.33%), Space: 55.1 MB (16.01%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int coinChange(int[] coins, int M) {
+        if(M == 0)
+            return 0;
+        int N = coins.length;
+        int[][] dp = new int[N][M + 1];
+        
+        Arrays.stream(dp).forEach(a->Arrays.fill(a, Integer.MAX_VALUE/2));
+        dp[0][0] = 0;
+        
+        for(int i = 0; i < N; ++i)
+        {
+            for(int j = 0; j < M + 1; ++j)
+            {
+                if(i == 0 && j == 0)
+                {
+                    continue;
+                }
+                
+                if(i >= 1)
+                    dp[i][j] = dp[i - 1][j];
+                if(j >= coins[i])
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j - coins[i]] + 1);
+            }
+        }
+        
+        if(dp[N - 1][M] < 0 || dp[N - 1][M] > 10000)
+        {
+            return -1;
+        }
+        return dp[N - 1][M];
+    }
+}
