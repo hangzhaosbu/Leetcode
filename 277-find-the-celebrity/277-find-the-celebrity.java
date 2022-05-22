9d0ac5ec4d9d0e296575bc93a30bb2534faa4852
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-05-22 11:21:41 -0400

    Time: 23 ms (43.28%), Space: 54 MB (45.58%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.java
+++ 277-find-the-celebrity/277-find-the-celebrity.java
@@ -3,35 +3,24 @@
 
 public class Solution extends Relation {
     public int findCelebrity(int n) {
-        HashSet<Integer> hashset = new HashSet<>();
-        HashMap<Integer, Integer> hashmap = new HashMap<>();
-        for(int i = 0; i < n; ++i)
-        {
-            hashset.add(i);
-            hashmap.put(i, 0);
-        }
+        int candidate = 0;
         
         for(int i = 0; i < n; ++i)
         {
-            for(int j = 0; j < n; ++j)
+            if(knows(candidate, i))
             {
-                if(i == j)
-                {
-                    continue;
-                }
-                
-                if(knows(i, j))
-                {
-                    hashset.remove(i);
-                    hashmap.put(j, hashmap.get(j) + 1);
-                }
+                candidate = i;
             }
         }
         
-        if(hashset.size() != 1 || hashmap.get(hashset.iterator().next()) != n - 1)
+        for(int i = 0; i < n; ++i)
         {
-            return -1;
+            if(i != candidate && (knows(candidate, i) || !knows(i, candidate)))
+            {
+                return -1;
+            }
         }
-        return hashset.iterator().next();
+        
+        return candidate;
     }
 }
