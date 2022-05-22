9e761cfc11ab35170af750e51826de935cb1f0e3
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-05-22 11:07:13 -0400

    Time: 784 ms (5.02%), Space: 52.7 MB (69.88%) - LeetHub

---
+++ 277-find-the-celebrity/277-find-the-celebrity.java
@@ -0,0 +1,44 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        HashSet<Integer> hashset = new HashSet<>();
+        HashMap<Integer, Integer> hashmap = new HashMap<>();
+        for(int i = 0; i < n; ++i)
+        {
+            hashset.add(i);
+            hashmap.put(i, 0);
+        }
+        
+        for(int i = 0; i < n; ++i)
+        {
+            for(int j = 0; j < n; ++j)
+            {
+                if(i == j)
+                {
+                    continue;
+                }
+                
+                if(knows(i, j))
+                {
+                    hashset.remove(i);
+                    hashmap.put(j, hashmap.get(j) + 1);
+                }
+            }
+        }
+        
+        if(hashset.size() != 1)
+        {
+            return -1;
+        }
+        
+        int c = hashset.iterator().next();
+        
+        if(hashmap.get(c) != n - 1)
+        {
+            return -1;
+        }
+        return c;
+    }
+}
