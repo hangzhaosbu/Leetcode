e65f2442887b6cfbddbfc6c2ef9e53ce50cc57aa
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-05-22 19:06:05 -0400

    Time: 4 ms (19.67%), Space: 45.2 MB (27.92%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -2,19 +2,17 @@ class Solution {
     public int maxProduct(int[] nums) {
         int N = nums.length;
         
-        int[] pdp = new int[N + 1];
-        int[] ndp = new int[N + 1];
-        
-        pdp[0] = 1;
-        ndp[0] = 1;
+        int pdp = 1;
+        int ndp = 1;
         
         int result = Integer.MIN_VALUE;
         for(int i = 1; i < N + 1; ++i)
         {
-            pdp[i] = Math.max(nums[i - 1], Math.max(pdp[i - 1] * nums[i - 1], ndp[i - 1] * nums[i - 1]));
-            ndp[i] = Math.min(nums[i - 1], Math.min(ndp[i - 1] * nums[i - 1], pdp[i - 1] * nums[i - 1]));
+            int tpdp = pdp, tndp = ndp;
+            pdp = Math.max(nums[i - 1], Math.max(tpdp * nums[i - 1], tndp * nums[i - 1]));
+            ndp = Math.min(nums[i - 1], Math.min(tndp * nums[i - 1], tpdp * nums[i - 1]));
             
-            result = Math.max(result, Math.max(pdp[i], ndp[i]));
+            result = Math.max(result, Math.max(pdp, ndp));
         }
         
         return result;
