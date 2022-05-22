a0bccefd09c990a5a52e0ee629e6e41763b60bd4
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-05-22 11:22:11 -0400

    Time: 23 ms (43.28%), Space: 53.7 MB (60.68%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.java
+++ 277-find-the-celebrity/277-find-the-celebrity.java
@@ -15,7 +15,7 @@ public class Solution extends Relation {
         
         for(int i = 0; i < n; ++i)
         {
-            if(i != candidate && (knows(candidate, i) || !knows(i, candidate)))
+            if(i != candidate && knows(candidate, i) || !knows(i, candidate))
             {
                 return -1;
             }
