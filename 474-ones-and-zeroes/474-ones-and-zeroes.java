ff993aa334b060b3c5cd495138137d8f0c8d5784
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-05-22 22:16:03 -0400

    Time: 292 ms (19.90%), Space: 79.6 MB (40.69%) - LeetHub

---
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -0,0 +1,60 @@
+class Solution {
+    private int[] cal_zero_one(String s)
+    {
+        int N = s.length();
+        int zeros = 0, ones = 0;
+        
+        for(int i = 0; i < N; ++i)
+        {
+            if(s.charAt(i) == '0')
+            {
+                zeros++;
+            }
+            else
+            {
+                ones++;
+            }
+        }
+        
+        return new int[]{zeros, ones};
+    }
+    
+    private int dfs(int[][] count, int start, int N, int m, int n, int[][][] dp)
+    {
+        if(start == N)
+        {
+            return 0;
+        }
+        
+        if(dp[m][n][start] != 0)
+        {
+            return dp[m][n][start];
+        }
+        
+        int taken = 0;
+        
+        if(m >= count[start][0] && n >= count[start][1])
+        {
+            taken = dfs(count, start + 1, N, m - count[start][0], n - count[start][1], dp) + 1;
+        }
+        
+        int not_taken = dfs(count, start + 1, N, m, n, dp);
+        
+        dp[m][n][start] = Math.max(taken, not_taken);
+        return dp[m][n][start];
+    }
+    
+    public int findMaxForm(String[] strs, int m, int n)
+    {
+        int N = strs.length;
+        int[][] count = new int[N][N];
+        int[][][] dp = new int[m + 1][n + 1][N];
+        
+        for(int i = 0; i < N; i++)
+        {
+            count[i] = cal_zero_one(strs[i]);
+        }
+        
+        return dfs(count, 0, N, m, n, dp);
+    }
+}
