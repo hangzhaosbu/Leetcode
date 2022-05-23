05fec32cb6d795b87f6772624effa8f1a2a80f23
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-05-22 20:16:11 -0400

    Time: 1295 ms (5.01%), Space: 131 MB (5.01%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -0,0 +1,58 @@
+class Solution {
+    
+    boolean result = false;
+    int[][] dp;
+    
+    private int dfs(int start, int running_sum, int sum, int[] nums)
+    {
+        if(dp[start][running_sum] != -1)
+        {
+            return dp[start][running_sum];
+        }
+        
+        int N = nums.length;
+        
+        if(start == N)
+        {
+            return 0;
+        }
+        if(running_sum == sum - running_sum)
+        {
+            result = true;
+            dp[start][running_sum] = 1;
+            return 1;
+        }
+        
+        int r = 0;
+        for(int i = start; i < N; ++i)
+        {
+            if(dfs(i + 1, running_sum + nums[i], sum, nums) == 1)
+            {
+                r = 1;
+                break;
+            }
+        }
+        
+        dp[start][running_sum] = r;
+        return r;
+    }
+    
+    public boolean canPartition(int[] nums)
+    {
+        int sum = 0;
+        int N = nums.length;
+        dp = new int[N + 1][N * 100 + 1];
+        
+        Arrays.stream(dp).forEach(a->Arrays.fill(a, -1));
+        // Arrays.fill(dp, -1);
+        
+        for(int num : nums)
+        {
+            sum += num;
+        }
+        dfs(0, 0, sum, nums);
+        
+        
+        return result;
+    }
+}
