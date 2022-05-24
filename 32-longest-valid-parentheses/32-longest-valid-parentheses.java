e592aaa6aa8c05399e7a82cb9f795ca3600fa3df
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-05-23 21:52:19 -0400

    Time: 10 ms (18.71%), Space: 42.3 MB (80.77%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -8,12 +8,9 @@ class Solution {
         {
             if(!stack.empty() && s.charAt(stack.peek()) == '(' && s.charAt(i) == ')')
             {
-                int right = i;
-                int left = stack.peek();
                 stack.pop();
-                
-                left = stack.empty() ? -1 : Math.min(left, stack.peek());
-                count = Math.max(count, right - left);
+                int left = stack.empty() ? -1 : stack.peek();
+                count = Math.max(count, i - left);
             }
             else
             {
