249b6408c86ec164c1abb2930f3663c30fbed0c4
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-06-14 18:32:23 -0400

    Time: 171 ms (29.07%), Space: 47.7 MB (75.94%) - LeetHub

---
+++ 15-3sum/15-3sum.java
@@ -0,0 +1,61 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        HashSet<List<Integer>> hashset = new HashSet<>();
+        
+        if(nums.length < 3)
+        {
+            return result;
+        }
+        Arrays.sort(nums);
+        int N = nums.length;
+        int i = 0;
+        
+        while(i < N)
+        {
+            if(i > 0 && nums[i] == nums[i - 1])
+            {
+                i++;    
+            }
+            
+            if(i < N)
+            {
+                int target = 0 - nums[i];
+                int left = i + 1, right = N - 1;
+
+
+                while(left < right)
+                {
+                    List<Integer> curt = new ArrayList<>();
+
+                    if(left < right && nums[left] + nums[right] > target)
+                    {
+                        right--;
+                    }
+                    else if(left < right && nums[left] + nums[right] < target)
+                    {
+                        left++;
+                    }
+                    else if(left < right && nums[left] + nums[right] == target)
+                    {
+                        curt.add(nums[i]);
+                        curt.add(nums[left]);
+                        curt.add(nums[right]);
+                        if(!hashset.contains(curt))
+                        {
+                            hashset.add(curt);
+                            result.add(curt);
+                        }
+                            
+                        left++;
+                        right--;
+                    }
+                }
+            }
+            
+            i++;
+        }
+        
+        return result;
+    }
+}
