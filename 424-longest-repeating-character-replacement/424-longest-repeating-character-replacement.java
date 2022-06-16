5d9198f9a803d1c72730e9521d2922d891656437
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-06-15 22:31:23 -0400

    Time: 13 ms (54.62%), Space: 42.3 MB (85.81%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -1,22 +1,33 @@
 class Solution {
     
+    private boolean valid(int[] hashmap, int k)
+    {
+        int max_val = 0, sum = 0;
+        
+        for(int num : hashmap)
+        {
+            max_val = Math.max(max_val, num);
+            sum += num;
+        }
+        
+        return sum - max_val <= k;
+    }
+    
     public int characterReplacement(String s, int k) {
         int N = s.length();
         int[] hashmap = new int[26];
         int result = 0;
-        int j = 0, sum = 0;
         
+        int j = 0;
         for(int i = 0; i < N; ++i)
         {
-            while(j < N && sum - Arrays.stream(hashmap).max().getAsInt() <= k)
+            while(j < N && valid(hashmap, k))
             {
-                sum++;
                 hashmap[s.charAt(j++) - 'A']++;
             }
             
-            result = Math.max(result, (sum - Arrays.stream(hashmap).max().getAsInt() <= k) ? j - i : j - i - 1);
+            result = Math.max(result, valid(hashmap, k) ? j - i : j - i - 1);
             hashmap[s.charAt(i) - 'A']--;
-            sum--;
         }
         
         return result;
