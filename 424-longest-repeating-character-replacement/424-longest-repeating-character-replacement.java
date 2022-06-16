6221e45ecea59a50355121b497b4ba22ac6a904c
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-06-15 22:22:12 -0400

    Time: 32 ms (33.56%), Space: 43.6 MB (40.83%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -1,23 +1,50 @@
 class Solution {
+    
+    private boolean valid(int[] hashmap, int j, int k, String s)
+    {
+        int max_val = 0, flag = 0;
+        int count = 0;
+        
+        for(int num : hashmap)
+        {
+            max_val = Math.max(max_val, num);
+        }
+        
+        for(int num : hashmap)
+        {
+            if(num == max_val && flag == 0)
+            {
+                flag = 1;
+            }
+            else
+            {
+                count += num;
+            }
+        }
+        
+        return count <= k;
+    }
+    
     public int characterReplacement(String s, int k) {
         int N = s.length();
+        int[] hashmap = new int[26];
         int result = 0;
         
-        for(int p = 'A'; p <= 'Z'; ++p)
+        int j = 0;
+        for(int i = 0; i < N; ++i)
         {
-            int j = -1;
-            int count = k;
-            for(int i = 0; i < N; ++i)
+            while(j < N && valid(hashmap, j, k, s))
             {
-                while(j + 1 < N && (s.charAt(j + 1) == p || (s.charAt(j + 1) != p && count > 0)))
-                {
-                    count -= s.charAt(j + 1) == p ? 0 : 1;
-                    j++;
-                }
-                
-                result = Math.max(result, j - i + 1);
-                count += s.charAt(i) == p ? 0 : 1;
+                hashmap[s.charAt(j++) - 'A']++;
             }
+            
+            if(valid(hashmap, j, k, s))
+                result = Math.max(result, j - i);
+            else
+            {
+                result = Math.max(result, j - i - 1);
+            }
+            hashmap[s.charAt(i) - 'A']--;
         }
         
         return result;
