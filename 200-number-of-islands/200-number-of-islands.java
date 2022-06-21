37787fd0aff7495371ee5dbd6c2a34396499c32a
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-06-21 13:53:33 -0400

    Time: 4 ms (73.38%), Space: 57.7 MB (36.06%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -13,7 +13,7 @@ class Solution {
                 if(grid[i][j] == '1')
                 {
                     count++;
-                    dfs(i, j, grid);
+                    dfs(i, j, N, M, grid);
                 }
             }
         }
@@ -21,15 +21,9 @@ class Solution {
         return count;
     }
     
-    private void dfs(int x, int y, char[][] grid)
+    private void dfs(int x, int y, int N, int M, char[][] grid)
     {
-        int N = grid.length, M = grid[0].length;
-        if(x < 0 || x >= N || y < 0 || y >= M)
-        {
-            return;
-        }
-        
-        if(grid[x][y] == '0')
+        if(x < 0 || x >= N || y < 0 || y >= M || grid[x][y] == '0')
         {
             return;
         }
@@ -39,7 +33,7 @@ class Solution {
         for(int i = 0; i < 4; ++i)
         {
             int new_x = x + directions[i][0], new_y = y + directions[i][1];
-            dfs(new_x, new_y, grid);
+            dfs(new_x, new_y, N, M, grid);
         }
     }
 }
