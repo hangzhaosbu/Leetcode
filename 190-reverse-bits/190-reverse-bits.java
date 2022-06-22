68d10c43ae3498ab55b36d98ecc9a8bacacda8b2
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-06-22 10:13:29 -0400

    Time: 1 ms (98.48%), Space: 42.5 MB (45.60%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.java
@@ -0,0 +1,19 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        
+        for(int i = 0; i < 32; i++)
+        {
+            result = result << 1;
+            if((n & 1) == 1)
+            {
+                result ++;
+            }
+            
+            n = n >> 1;
+        }
+        
+        return result;
+    }
+}
