7e55eec17653d386017e9eb85cfc8d481078729f
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-06-21 23:39:46 -0400

    Time: 5 ms (36.86%), Space: 67.5 MB (67.35%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean canJump(int[] nums) {
         int max = 0;
         
-        for(int i = 0; i <= Math.min(max, nums.length - 1); ++i)
+        for(int i = 0; i < Math.min(max + 1, nums.length); ++i)
         {
             max = Math.max(max, i + nums[i]);
         }
