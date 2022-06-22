0b09511d0ced4417c168fd9eed840e36482f8bf3
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-06-22 11:46:53 -0400

    Time: 16 ms (5.09%), Space: 49.3 MB (6.17%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,12 +1,21 @@
 class Solution {
+    
+    private int[][] getResult(TreeMap<Integer, Integer> treemap)
+    {
+        int[][] result = new int[treemap.size()][2];
+        int p = 0;
+
+        while(!treemap.isEmpty())
+        {
+            result[p++] = new int[]{treemap.firstEntry().getKey(), treemap.firstEntry().getValue()};
+            treemap.pollFirstEntry();
+        }
+        return result;
+    }
+    
     public int[][] insert(int[][] intervals, int[] newInterval)
     {
-        /*
-            |   |
-              |   |
-        */
         TreeMap<Integer, Integer> treemap = new TreeMap<Integer, Integer>();
-        
         for(int[] interval : intervals)
         {
             treemap.put(interval[0], interval[1]);
@@ -18,18 +27,9 @@ class Solution {
         Map.Entry<Integer, Integer> floor = treemap.floorEntry(left);
         if(floor != null){
             
-            if(floor.getValue() >= end)
+            if(floor.getValue() > end - 1)
             {
-                int[][] result = new int[treemap.size()][2];
-                int p = 0;
-
-                while(!treemap.isEmpty())
-                {
-                    result[p++] = new int[]{treemap.firstEntry().getKey(), treemap.firstEntry().getValue()};
-                    treemap.pollFirstEntry();
-                }
-                
-                return result;
+                return getResult(treemap);
             }
             
             if(floor.getValue() >= start){
@@ -38,25 +38,14 @@ class Solution {
             }
         }
         
-        
         Map.Entry<Integer, Integer> ceiling = treemap.ceilingEntry(left);
-        while(ceiling != null && ceiling.getKey() <= end){
+        while(ceiling != null && ceiling.getKey() < end + 1){
             treemap.remove(ceiling.getKey());
             end = Math.max(end, ceiling.getValue());
             ceiling = treemap.ceilingEntry(left);
         }
         
         treemap.put(start, end);
-        
-        int[][] result = new int[treemap.size()][2];
-        int p = 0;
-        
-        while(!treemap.isEmpty())
-        {
-            result[p++] = new int[]{treemap.firstEntry().getKey(), treemap.firstEntry().getValue()};
-            treemap.pollFirstEntry();
-        }
-        
-        return result;
+        return getResult(treemap);
     }
 }
