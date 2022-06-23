39f328c4f04dbadc33f0a5174f4140258995ff98
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-06-22 20:52:45 -0400

    Time: 1 ms (99.71%), Space: 44.3 MB (98.21%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,51 +1,28 @@
 class Solution {
-    public int[][] insert(int[][] intervals, int[] newInterval) {
-        int N = intervals.length;
-        int[][] candidates = new int[N + 1][2];
+    public int[][] insert(int[][] intervals, int[] newInterval)
+    {
+        List<int[]> result = new ArrayList<>();
         
-        for(int i = 0; i < N; i++)
+        for(int[] interval : intervals)
         {
-            candidates[i][0] = intervals[i][0];
-            candidates[i][1] = intervals[i][1];
-        }
-        
-        candidates[N][0] = newInterval[0];
-        candidates[N][1] = newInterval[1];
-        
-        Arrays.sort(candidates, new Comparator<int[]>(){
-            public int compare(int[] a, int[] b){
-                return Integer.compare(a[0], b[0]);
-            }
-        });
-        
-        Stack<int[]> stack = new Stack<>();
-        
-        for(int i = 0; i < N + 1; i++)
-        {
-            int[] curt = candidates[i];
-            int start = curt[0], end = curt[1];
-            
-            while(!stack.isEmpty() && stack.peek()[1] >= start)
+            int start = interval[0], end = interval[1];
+            if(end < newInterval[0])
             {
-                int prev_start = stack.peek()[0], prev_end = stack.peek()[1];
-                stack.pop();
-                
-                start = Math.min(start, prev_start);
-                end = Math.max(end, prev_end);
+                result.add(interval);
             }
-        
-            stack.add(new int[]{start, end});
+            else if(start > newInterval[1])
+            {
+                result.add(newInterval);
+                newInterval = new int[]{start, end};
+            }
+            else
+            {
+                newInterval[0] = Math.min(newInterval[0], start);
+                newInterval[1] = Math.max(newInterval[1], end);
+            }
         }
         
-        int[][] result = new int[stack.size()][2];
-        int p = stack.size() - 1;
-        
-        while(!stack.isEmpty())
-        {
-            result[p--] = new int[]{stack.peek()[0], stack.peek()[1]};
-            stack.pop();
-        }
-        
-        return result;
+        result.add(newInterval);
+        return result.toArray(new int[result.size()][2]);
     }
 }
