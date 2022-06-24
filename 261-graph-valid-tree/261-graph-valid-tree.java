70863d2189be9fd1291177f2c2c90c8b90fd3ba4
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-06-24 12:00:21 -0400

    Time: 2 ms (63.82%), Space: 46 MB (75.75%) - LeetHub

---
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -0,0 +1,75 @@
+class UnionFind
+{
+    int[] parent;
+    int count = 0;
+    
+    public UnionFind(int n)
+    {
+        parent = new int[n];
+        count = n;
+        
+        for(int i = 0; i < n; ++i)
+        {
+            parent[i] = i;
+        }
+    }
+    
+    public int find(int x)
+    {
+        if(x != parent[x])
+        {
+            parent[x] = find(parent[x]);
+        }
+        
+        return parent[x];
+    }
+    
+    public boolean unionfind(int x, int y)
+    {
+        int px = find(x), py = find(y);
+        
+        if(px != py)
+        {
+            if(px < py)
+            {
+                parent[px] = py;
+            }
+            else
+            {
+                parent[py] = px;
+            }
+            
+            count--;
+            return true;
+        }
+        
+        return false;
+    }
+}
+
+class Solution
+{
+    public boolean validTree(int n, int[][] edges)
+    {
+        UnionFind uf = new UnionFind(n);
+        
+        for(int[] edge : edges)
+        {
+            int x = edge[0], y = edge[1];
+            
+            if(uf.unionfind(x, y) == false)
+            {
+                return false;
+            }
+        }
+        
+        // for(int i = 0; i < n; ++i)
+        // {
+        //     uf.find(i);
+        // }
+        
+        
+        
+        return uf.count == 1;
+    }
+}
