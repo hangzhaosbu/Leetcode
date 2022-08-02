a3fc3dfaf23fb6ba0cd156bbb2571a6f702fd8ba
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-08-01 23:42:44 -0400

    Time: 1 ms (66.28%), Space: 42.3 MB (64.25%) - LeetHub

---
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    private List<List<Integer>> solution;
+    
+    private int getHeight(TreeNode root) {
+        
+        // return -1 for null nodes
+        if (root == null) {
+            return -1;
+        }
+        
+        // first calculate the height of the left and right children
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+        
+        int currHeight = Math.max(leftHeight, rightHeight) + 1;
+        
+        if (this.solution.size() == currHeight) {
+            this.solution.add(new ArrayList<>());
+        }
+        
+        this.solution.get(currHeight).add(root.val);
+        
+        return currHeight;
+    }
+    
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        this.solution = new ArrayList<>();
+        
+        getHeight(root);
+        
+        return this.solution;
+    }
+}
