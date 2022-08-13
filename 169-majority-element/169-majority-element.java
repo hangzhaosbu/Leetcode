27336fdf4d704b0d0add400fd8d95af2ab0942c3
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-08-13 09:35:53 -0400

    Time: 3 ms (63.74%), Space: 56 MB (52.96%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -3,23 +3,10 @@ class Solution {
         int candidate = nums[0];
         int count = 1;
         
-        
         for(int i = 1; i < nums.length; ++i)
         {
-            if(count == 0)
-            {
-                candidate = nums[i];
-            }
-            
-            
-            if(nums[i] == candidate)
-            {
-                count++;
-            }
-            else
-            {
-                count--;
-            }
+            candidate = count == 0 ? nums[i] : candidate;
+            count += nums[i] == candidate ? 1 : -1;
         }
         
         return candidate;
