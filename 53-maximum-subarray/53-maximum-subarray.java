4e001bc4af656a102ac03289b9233c2fc9af1efe
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-08-13 10:42:58 -0400

    Time: 2 ms (74.13%), Space: 73.1 MB (74.53%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,46 +1,24 @@
 class Solution {
-    
-    int[] nums;
-    
-    public int maxSubArray(int[] nums)
-    {
-        this.nums = nums;
-        
-        return divideAndConquer(0, nums.length - 1);
-    }
-    
-    private int divideAndConquer(int left, int right)
-    {
-        if(left > right)
-        {
-            return Integer.MIN_VALUE;
-        }
-        
-        int mid = (left + right) / 2;
+    public int maxSubArray(int[] nums) {
+        //[-2,1,-3,4,-1,2,1,-5,4]
+        //    A      |     B
+        //    A' C   | D   B'
         
         int sum = 0;
-        int bestright = 0;
-        int bestleft = 0;
+        int result = Integer.MIN_VALUE;
         
-        for(int i = mid + 1; i <= right; ++i)
+        for(int num : nums)
         {
-            sum += this.nums[i];
-            bestright = Math.max(bestright, sum);
+            sum += num;
+            
+            if(sum < num)
+            {
+                sum = num;
+            }
+            
+            result = Math.max(result, sum);
         }
         
-        sum = 0;
-        
-        for(int i = mid - 1; i >= left; --i)
-        {
-            sum += this.nums[i];
-            bestleft = Math.max(bestleft, sum);
-        }
-        
-        int best = bestleft + nums[mid] + bestright;
-        
-        int l = divideAndConquer(left, mid - 1);
-        int r = divideAndConquer(mid + 1, right);
-
-        return Math.max(best, Math.max(l, r));
+        return result;
     }
 }
