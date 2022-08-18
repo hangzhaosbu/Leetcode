25af556df222041d587372a86d61ca9800e23ab0
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-08-18 10:13:51 -0400

    Time: 20 ms (15.63%), Space: 55 MB (69.31%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,39 +1,21 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        
+        Stack<int[]> stack = new Stack<>();
         Arrays.sort(intervals, new Comparator<>(){
             public int compare(int[] a, int[] b)
             {
-                if(a[1] != b[1])
-                {
-                    return a[1] - b[1];
-                }
-                else
-                {
-                    return a[0] - b[0];
-                }
+                return a[1] != b[1]? a[1] - b[1]: a[0] - b[0];
             }
         });
         
-        Stack<int[]> stack = new Stack<>();
-        
-        
         for(int i = 0; i < intervals.length; ++i)
         {
-            if(stack.isEmpty())
+            while(!stack.isEmpty() && stack.peek()[1] >= intervals[i][0])
             {
-                
-            }
-            
-            else
-            {
-                while(!stack.isEmpty() && stack.peek()[1] >= intervals[i][0])
-                {
-                    int[] top = stack.pop();
-                    
-                    intervals[i][0] = Math.min(intervals[i][0], top[0]);
-                    intervals[i][1] = Math.max(intervals[i][1], top[1]);
-                }
+                int[] top = stack.pop();
+
+                intervals[i][0] = Math.min(intervals[i][0], top[0]);
+                intervals[i][1] = Math.max(intervals[i][1], top[1]);
             }
             
             stack.add(intervals[i]);
