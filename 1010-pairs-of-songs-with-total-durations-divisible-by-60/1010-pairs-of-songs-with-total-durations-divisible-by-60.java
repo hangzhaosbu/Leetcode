13510969daa0de91f0157818c2e89dbd6cf17357
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-08-18 19:01:28 -0400

    Time: 97 ms (5.31%), Space: 80.1 MB (5.35%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,18 +1,9 @@
 class Solution {
     public int numPairsDivisibleBy60(int[] time)
     {
-        
-        // 2147483647, 
-        // 1799970000
         HashMap<Integer, Integer> hashmap = new HashMap<>();
-        int[] sixties = new int[16];
+        
         long result = 0;
-        
-        for(int i = 0; i < 16; ++i)
-        {
-            sixties[i] = (i + 1) * 60;
-        }
-        
         for(int i = 0; i < time.length; ++i)
         {
             hashmap.put(time[i], hashmap.getOrDefault(time[i], 0) + 1);
@@ -22,11 +13,12 @@ class Solution {
         {
             for(int j = 0; j < 16; ++j)
             {
-                if(sixties[j] - time[i] >= 0 && hashmap.containsKey(sixties[j] - time[i]))
+                int curt = (j + 1) * 60;
+                if(curt - time[i] >= 0 && hashmap.containsKey(curt - time[i]))
                 {
-                    result += hashmap.get(sixties[j] - time[i]);
+                    result += hashmap.get(curt - time[i]);
                     
-                    if(sixties[j] - time[i] == time[i])
+                    if(curt - time[i] == time[i])
                     {
                         result--;
                     }
