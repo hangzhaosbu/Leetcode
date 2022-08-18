a24597310d6c5fa5dee7d26cb5f04ebcfb174d22
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-08-18 11:53:32 -0400

    Time: 28 ms (6.77%), Space: 54.7 MB (82.39%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,32 +1,44 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        Stack<int[]> stack = new Stack<>();
-        Arrays.sort(intervals, new Comparator<>(){
-            public int compare(int[] a, int[] b)
-            {
-                return a[1] != b[1]? a[1] - b[1]: a[0] - b[0];
-            }
-        });
+    public int[][] merge(int[][] intervals)
+    {
+        TreeMap<Integer, Integer> treemap = new TreeMap<>();
         
         for(int i = 0; i < intervals.length; ++i)
         {
-            while(!stack.isEmpty() && stack.peek()[1] >= intervals[i][0])
+            int start = intervals[i][0], end = intervals[i][1];
+            Map.Entry<Integer, Integer> ceiling = treemap.ceilingEntry(start);
+            
+            while(ceiling != null && ceiling.getKey() <= end)
             {
-                int[] top = stack.pop();
-
-                intervals[i][0] = Math.min(intervals[i][0], top[0]);
-                intervals[i][1] = Math.max(intervals[i][1], top[1]);
+                start = Math.min(start, ceiling.getKey());
+                end = Math.max(end, ceiling.getValue());
+                treemap.remove(ceiling.getKey());
+                ceiling = treemap.ceilingEntry(start);
             }
             
-            stack.add(intervals[i]);
+            Map.Entry<Integer, Integer> floor = treemap.floorEntry(end);
+            
+            while(floor != null && floor.getValue() >= start)
+            {
+                start = Math.min(start, floor.getKey());
+                end = Math.max(end, floor.getValue());
+                treemap.remove(floor.getKey());
+                floor = treemap.floorEntry(end);
+            }
+            
+            treemap.put(start, end);
         }
         
-        int[][] result = new int[stack.size()][2];
-        int p = stack.size() - 1;
+        int size = treemap.size();
+        int[][] result = new int[size][2];
+        int p = 0;
         
-        while(!stack.isEmpty())
+        while(size-- > 0)
         {
-            result[p--] = stack.pop();
+            result[p][0] = treemap.firstEntry().getKey();
+            result[p][1] = treemap.firstEntry().getValue();
+            p++;
+            treemap.pollFirstEntry();
         }
         
         return result;
