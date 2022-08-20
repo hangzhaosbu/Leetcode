8227f4fff85cae57b71f29ad899b4d9e4c92b3a2
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-08-19 21:21:04 -0400

    Time: 1 ms (100.00%), Space: 73.7 MB (55.32%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,22 +1,11 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        //[-2,1,-3,4,-1,2,1,-5,4]
-        //    A      |     B
-        //    A' C   | D   B'
-        
-        int sum = 0;
-        int result = Integer.MIN_VALUE;
+        int sum = 0, result = Integer.MIN_VALUE;
         
         for(int num : nums)
         {
-            sum += num;
-            
-            if(sum < num)
-            {
-                sum = num;
-            }
-            
-            result = Math.max(result, sum);
+            sum = Math.max(sum + num, num);
+            result = Math.max(sum, result);
         }
         
         return result;
