5dd7d0682b059e2812d058cf57843bbb05b4fed2
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-08-21 13:11:49 -0400

    Time: 184 ms (32.27%), Space: 109.4 MB (25.15%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
@@ -1,20 +1,25 @@
 class Solution {
-    public int maxEvents(int[][] events)
-    {
+    public int maxEvents(int[][] events) {
         Arrays.sort(events, new Comparator<>(){
             public int compare(int[] a, int[] b)
             {
-                if(a[0] != b[0]) return a[0] - b[0];
-                else return a[1] - b[1];
+                if(a[0] != b[0])
+                {
+                    return a[0] - b[0];
+                }
+                else
+                {
+                    return a[1] - b[1];
+                }
             }
         });
         
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int N = events.length, i = 0, result = 0;
+        int i = 0, N = events.length, result = 0;
+        
         
         for(int day = 1; day <= 100000; day++)
         {
-            
             while(i < N && events[i][0] <= day)
             {
                 pq.offer(events[i][1]);
