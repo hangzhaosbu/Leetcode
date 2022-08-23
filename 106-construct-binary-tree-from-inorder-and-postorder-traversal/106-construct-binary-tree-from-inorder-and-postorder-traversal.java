0611e3df658aeb61f13cd33afa91594693e7585b
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-08-23 10:32:30 -0400

    Time: 4 ms (60.77%), Space: 42.2 MB (91.09%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     
-    private TreeNode recursion(int inorder_left, int inorder_right, int[] inorder, int postorder_left, int postorder_right, int[] postorder)
+    private TreeNode recursion(int inorder_left, int inorder_right , int[] inorder, int postorder_left, int postorder_right, int[] postorder)
     {
         if(inorder_left > inorder_right || postorder_left > postorder_right)
         {
@@ -25,6 +25,8 @@ class Solution {
         int node_val = postorder[postorder_right];
         int node_index = inorder_left;
         
+        TreeNode node = new TreeNode(node_val);
+        
         while(inorder[node_index] != node_val)
         {
             node_index++;
@@ -33,11 +35,8 @@ class Solution {
         int left_length = node_index - inorder_left;
         int right_length = inorder_right - node_index;
         
-        TreeNode node = new TreeNode(node_val);
-        
-        
         TreeNode left = recursion(inorder_left, node_index - 1, inorder, postorder_right - right_length - left_length, postorder_right - right_length - 1, postorder);
-        TreeNode right = recursion(node_index + 1, inorder_right, inorder, postorder_right - right_length,  postorder_right - 1, postorder);
+        TreeNode right = recursion(node_index + 1, inorder_right, inorder, postorder_right - right_length, postorder_right - 1, postorder);
         
         node.left = left;
         node.right = right;
