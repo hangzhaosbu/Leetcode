fa918bf41650f344154b73b53a8d40446a102141
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-08-22 21:09:31 -0400

    Time: 22 ms (26.98%), Space: 102.8 MB (35.61%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -9,49 +9,22 @@
  * }
  */
 class Solution {
+    
+    ListNode first = null;
+    
     public boolean isPalindrome(ListNode head)
     {
-        ListNode prev = null, next = null;
-        ListNode curt = head;
-        int count = 0;
-        
-        while(curt != null)
+        first = head;
+        return recursion(head);
+    }
+    
+    private boolean recursion(ListNode node)
+    {
+        if(node != null)
         {
-            curt = curt.next;
-            count ++;
-        }
-        
-        count = count / 2;
-        
-        int len = count;
-        
-        curt = head;
-        
-        while(count > 0)
-        {
-            curt = curt.next;
-            count--;
-        }
-        
-        while(curt != null)
-        {
-            next = curt.next;
-            curt.next = prev;
-            prev = curt;
-            curt = next;
-        }
-        
-        while(len > 0)
-        {
-            if(prev.val == head.val)
-            {
-                prev = prev.next;
-                head = head.next;
-            }
-            else{
-                return false;
-            }
-            len --;
+            if(!recursion(node.next)) return false;
+            if(node.val != first.val) return false;
+            first = first.next;
         }
         
         return true;
