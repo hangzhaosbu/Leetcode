681a4d8f451bb97d14cfb16d059b535c16764210
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-08-25 09:37:02 -0400

    Time: 1 ms (100.00%), Space: 41.8 MB (15.71%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        //log4(n) = log10(n)/log10(4)
-        return Math.log10(n)/Math.log10(4) % 1 == 0;
+        return Math.log10(n) / Math.log10(4) % 1 == 0;
     }
 }
