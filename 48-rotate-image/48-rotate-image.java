3c7e92bf8bd7074bb1b093a8402caada7c42cccd
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-08-29 23:11:59 -0400

    Time: 0 ms (100.00%), Space: 41.2 MB (89.14%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i = 0; i < matrix.length; ++i)
+        {
+            for(int j = 0; j <= i; ++j)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        for(int i = 0; i < matrix.length; ++i)
+        {
+            for(int j = 0; j < matrix[0].length / 2; ++j)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[i].length - j - 1];
+                matrix[i][matrix[i].length - j - 1] = temp;
+            }
+        }
+        
+    }
+}
