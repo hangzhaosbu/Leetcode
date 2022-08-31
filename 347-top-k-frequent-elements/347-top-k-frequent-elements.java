c0a8394679685511b69549368556b2fd4bac2e42
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-08-31 11:55:13 -0400

    Time: 22 ms (37.33%), Space: 50.5 MB (56.65%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,6 +1,23 @@
 class Solution {
-    //  S S S S O O O O L L L L L
-    //          i t   j
+    public int[] topKFrequent(int[] nums, int k)
+    {
+        HashMap<Integer, Integer> hashmap = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; ++i)
+        {
+            hashmap.put(nums[i], hashmap.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        int[][] count = new int[hashmap.size()][2];
+        int p = 0;
+        
+        for(int key : hashmap.keySet())
+        {
+            count[p++] = new int[]{key, hashmap.get(key)};
+        }
+        
+        return quickselect(0, count.length - 1, count, k);
+    }
     
     private int[] quickselect(int left, int right, int[][] count, int k)
     {
@@ -8,7 +25,6 @@ class Solution {
         {
             return null;
         }
-        
         
         int mid = (right - left) / 2 + left;
         int pivot = count[mid][1];
@@ -26,7 +42,6 @@ class Solution {
                 i++;
                 t++;
             }
-            
             else if(count[t][1] > pivot)
             {
                 int[] temp = count[t];
@@ -35,67 +50,50 @@ class Solution {
                 
                 j--;
             }
-            
             else
             {
                 t++;
             }
         }
         
+        // S S S S S O O O L L L L
+        //           i t j
+        
         if(right - j + 1 == k)
         {
             int[] result = new int[k];
-            int p = 0;
+            int q = 0;
             
-            for(int k1 = j; k1 <= right; ++k1)
+            for(int p = j; p <= right; p++)
             {
-                result[p++] = count[k1][0];
+                result[q++] = count[p][0];
             }
+            
             return result;
         }
-        
         else if(right - j + 1 > k)
         {
             return quickselect(j + 1, right, count, k);
         }
-        
         else
         {
-            int[] left_part = quickselect(left, j - 1, count, k - (right - j + 1));
+            int[] result = new int[k];
+            int q = 0;
             
-            int[] result = new int[left_part.length + (right - j + 1)];
-            int p = 0;
             
-            for(int k1 = 0; k1 < left_part.length; ++k1)
+            int[] l = quickselect(left, j - 1, count, k - (right - j + 1));
+            
+            for(int p = 0; p < l.length; p++)
             {
-                result[p++] = left_part[k1];
+                result[q++] = l[p];
             }
             
-            for(int k1 = j; k1 <= right; ++k1)
+            for(int p = j; p <= right; p++)
             {
-                result[p++] = count[k1][0];
+                result[q++] = count[p][0];
             }
             
             return result;
         }
-    }
-    
-    public int[] topKFrequent(int[] nums, int k)
-    {
-        HashMap<Integer, Integer> hashmap = new HashMap<>();
-        
-        for(int i = 0; i < nums.length; ++i)
-        {
-            hashmap.put(nums[i], hashmap.getOrDefault(nums[i], 0) + 1);
-        }
-        
-        int[][] count = new int[hashmap.size()][2];
-        int p = 0;
-        
-        for(int key : hashmap.keySet())
-        {
-            count[p++] = new int[]{key, hashmap.get(key)};
-        }
-        return quickselect(0, hashmap.size() - 1, count, k);
     }
 }
