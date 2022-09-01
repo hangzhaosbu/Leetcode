087d0acaa0679f15e5d5251c1480303a98266224
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-09-01 19:29:41 -0400

    Time: 2 ms (87.57%), Space: 48.6 MB (13.99%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,13 +1,14 @@
 class Solution {
     public int[] countBits(int n) {
         int[] dp = new int[n + 1];
+        
         int count = 1;
         
-        for(int i = 1; i <= n ; ++i)
+        for(int i = 1; i <= n; ++i)
         {
             if(i == count * 2)
             {
-                count *= 2;
+                count = count * 2;
             }
             
             dp[i] = dp[i - count] + 1;
