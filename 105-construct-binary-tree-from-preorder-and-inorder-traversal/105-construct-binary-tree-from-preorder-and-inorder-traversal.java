55034ce88bcb1860127343bd623ec50d55dded6b
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-09-10 13:53:45 -0400

    Time: 4 ms (64.81%), Space: 44.1 MB (73.19%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,6 +14,10 @@
  * }
  */
 class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) 
+    {
+        return recursion(0, preorder.length - 1, preorder, 0, inorder.length - 1, inorder);
+    }
     
     private TreeNode recursion(int preorder_left, int preorder_right, int[] preorder, int inorder_left, int inorder_right, int[] inorder)
     {
@@ -23,10 +27,9 @@ class Solution {
         }
         
         int node_val = preorder[preorder_left];
-        int node_index = inorder_left;
-        
         TreeNode node = new TreeNode(node_val);
         
+        int node_index = inorder_left;
         while(inorder[node_index] != node_val)
         {
             node_index++;
@@ -35,17 +38,12 @@ class Solution {
         int left_length = node_index - inorder_left;
         int right_length = inorder_right - node_index;
         
-        TreeNode left = recursion(preorder_left + 1, preorder_left + 1 + left_length, preorder, inorder_left, node_index - 1, inorder);
-        TreeNode right = recursion(preorder_left + 1 + left_length, preorder_left + 1 + left_length + right_length, preorder, node_index + 1, inorder_right, inorder);
+        TreeNode left = recursion(preorder_left + 1, preorder_left + left_length, preorder, inorder_left, node_index - 1, inorder);
+        TreeNode right = recursion(preorder_left + left_length + 1, preorder_left + left_length + right_length, preorder, node_index + 1, inorder_right, inorder);
         
         node.left = left;
         node.right = right;
         
         return node;
-    }
-    
-    public TreeNode buildTree(int[] preorder, int[] inorder)
-    {
-        return recursion(0, preorder.length - 1, preorder, 0, inorder.length - 1, inorder);
     }
 }
