b1caab50904fd57b3698535d8b688c5682f80bde
Hang Zhao <37938674+Frannky@users.noreply.github.com>
2022-09-29 01:05:28 -0400

    Time: 11 ms (53.48%), Space: 62.7 MB (44.31%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -0,0 +1,55 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> result = new ArrayList<Integer>();
+        
+        // Base case
+        if (arr.length == k) {
+            for (int i = 0; i < k; i++) {
+                result.add(arr[i]);
+            }
+            
+            return result;
+        }
+        
+        // Binary search to find the closest element
+        int left = 0;
+        int right = arr.length;
+        int mid = 0;
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (arr[mid] >= x) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        // Initialize our sliding window's bounds
+        left -= 1;
+        right = left + 1;
+        
+        // While the window size is less than k
+        while (right - left - 1 < k) {
+            // Be careful to not go out of bounds
+            if (left == -1) {
+                right += 1;
+                continue;
+            }
+            
+            // Expand the window towards the side with the closer number
+            // Be careful to not go out of bounds with the pointers
+            if (right == arr.length || Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
+                left -= 1;
+            } else {
+                right += 1;
+            }
+        } 
+
+        // Build and return the window
+        for (int i = left + 1; i < right; i++) {
+            result.add(arr[i]);
+        }
+        
+        return result;
+    }
+}
