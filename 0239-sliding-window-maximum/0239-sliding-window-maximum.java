d8e3052efe5cfcb3544e72ea16b4aae8db3ee376
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2023-07-30 17:47:48 -0400

    Time: 14 ms (98.57%), Space: 59 MB (84.33%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,31 +1,64 @@
 class Solution {
-    final int N = (int) 1e5 + 5;
-    int[] maxq = new int[N];
-    int left = 0, right = 0;
-    
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int[] result = new int[nums.length - k + 1];
-        
-        for(int i = 0; i < nums.length; i++)
-        {
-            int curt = nums[i];
-            while(left != right && maxq[right - 1] < curt)
-            {
-                right--;
-            }
-            
-            maxq[right++] = curt;
-            
-            if(i - k >= 0 && nums[i - k] == maxq[left]) left++;;
-            
-            if(i >= k - 1)
-            {
-                result[i - k + 1] = maxq[left];
-            }
-            
-            
-        }
-        
-        return result;
+    private int[] arr = new int[99999];
+    private int pointer = 0;
+    private int first = 0;
+
+    private void offer(int x) {
+        arr[pointer] = x;
+        pointer++;
     }
+
+    private int poll() {
+        int x = arr[first];
+        first++;
+        return x;
+    }
+
+    private int peek() {
+        return arr[first];
+    }
+
+    private boolean empty() {
+        return pointer == first;
+    }
+
+    private int size() {
+        return pointer - first;
+    }
+
+   
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        if (nums.length <= 0 || k <= 0) {
+            return new int[0];
+        }
+
+
+
+        int[] ans = new int[nums.length - k + 1];
+
+        int index = 0;
+        for (int start = 0; start < nums.length; start++) {
+            while (!empty() && peek() < start - k + 1) {
+                poll();
+            }
+           
+       
+            while (!empty() && nums[start] >= nums[arr[pointer-1]]) {
+                pointer--;
+            }
+
+
+            offer(start);
+           
+           
+            if (start >= k-1){
+                ans[index] = nums[peek()];
+                index++;
+            }
+
+        }
+
+        return ans;
+    }  
 }
