fa3bdcbd155537c728fe8df874ef7c031e79c039
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2023-10-08 15:11:55 -0400

    Time: 4 ms (53.70%), Space: 8.7 MB (32.46%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -4,14 +4,14 @@ public:
         set<vector<int>> ans;
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        for(int i = pow(2, n); i < pow(2, n + 1); i++)
+        for(int i = pow(2, n); i < pow(2, n + 1); ++i)
         {
             string s = bitset<10>(i).to_string().substr(10 - n);
             vector<int> result;
             
             for(int i = 0; i < n; i++)
             {
-                if(s[i] == '1') result.push_back(nums[i]);
+                if(s[i] > '0') result.push_back(nums[i]);
             }
             if(ans.find(result) == ans.end())
             {
