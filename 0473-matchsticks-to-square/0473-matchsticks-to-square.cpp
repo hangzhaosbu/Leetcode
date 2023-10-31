d129fc4f41a397855ee83b8933f8e8b7bbf5f2e8
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2023-10-30 22:48:16 -0400

    Time: 395 ms (40.47%), Space: 10.4 MB (55.04%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool makesquare(vector<int>& A)
+    {
+        sort(A.begin(), A.end(), greater<int>());
+        vector<int> matchsticks(4, 0);
+        int total = 0;
+        
+        for(auto num : A) total += num;
+        
+        if(total % 4 != 0) return false;
+        
+        return dfs(0, total / 4, A, matchsticks);
+    }
+    
+    bool dfs(int start, int goal, vector<int>& A, vector<int>& matchsticks)
+    {
+        if(start == A.size())
+        {
+            return matchsticks[0] == matchsticks[1] && matchsticks[1] == matchsticks[2] && matchsticks[2] == matchsticks[3];
+        }
+        
+        for(int i = 0; i < 4; i++)
+        {
+            if(matchsticks[i] + A[start] > goal) continue;
+            matchsticks[i] += A[start];
+            if(dfs(start + 1, goal, A, matchsticks)) return true;;
+            matchsticks[i] -= A[start];
+        }
+        
+        return false;
+    }
+};
