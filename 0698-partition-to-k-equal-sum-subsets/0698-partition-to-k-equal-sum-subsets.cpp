9d8a3f359650e15411fe1657d0a20d4def772a82
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2023-10-31 13:40:19 -0400

    Time: 569 ms (33.54%), Space: 9.6 MB (81.49%) - LeetHub

---
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool valid(vector<int>& result, int k)
+    {
+        for(int i = 1; i < k; i++)
+        {
+            if(result[i] != result[i - 1]) return false;
+        }
+        return true;
+    }
+    
+    bool canPartitionKSubsets(vector<int>& nums, int k) 
+    {
+        int total = 0;
+        for(auto num : nums) total += num;
+        if(total % k != 0) return false;
+        vector<int> result(k, 0);
+        
+        // sort(nums.begin(), nums.end());
+        return dfs(0, k, total / k, nums, result);
+    }
+    
+    bool dfs(int start, int k, int goal, vector<int>& nums, vector<int>& result)
+    {
+        if(start == nums.size())
+        {
+            if(valid(result, k)) return true;
+            else return false;
+        }
+        
+        for(int i = 0; i < k; i++)
+        {
+            if(result[i] + nums[start] > goal) continue;
+            if(i > 0 && result[i] == result[i - 1]) continue;
+            
+            result[i] += nums[start];
+            if(dfs(start + 1, k, goal, nums, result)) return true;
+            result[i] -= nums[start];
+        }
+        
+        return false;
+    }
+};
