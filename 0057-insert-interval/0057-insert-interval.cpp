de7392ae6f2ecb826089dce28f7850c3b7709eca
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2023-11-27 22:35:59 -0500

    Time: 33 ms (6.28%), Space: 18.7 MB (5.06%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+        stack<vector<int>> stck;
+        
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){
+            return a[0] < b[0];
+        });
+        
+        for(int i = 0; i < intervals.size(); i++)
+        {
+            if(stck.empty())
+            {
+                stck.push(intervals[i]);
+            }
+            else
+            {
+                vector<int> curt = stck.top();
+                
+                
+                if(intervals[i][0] <= curt[1])
+                {
+                    stck.pop();
+                    int start = min(intervals[i][0], curt[0]);
+                    int end = max(intervals[i][1], curt[1]);
+                    stck.push({start, end});
+                }
+                else
+                {
+                    stck.push(intervals[i]);
+                }
+                
+            }
+        }
+        
+        while(!stck.empty())
+        {
+            ans.push_back(stck.top());
+            stck.pop();
+        }
+        
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
