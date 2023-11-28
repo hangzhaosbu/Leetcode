f5001c39767aeb9ebb08776ea51d613726271c0a
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2023-11-28 17:40:47 -0500

    Time: 113 ms (5.02%), Space: 68 MB (55.82%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,46 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int sum = 0;
-        int ans = INT_MIN;
-        
-        for(auto & num : nums)
+    int maxSubArray(vector<int>& nums)
+    {
+        return divideConquer(0, nums.size() - 1, nums);
+    }
+    
+    int divideConquer(int left, int right, vector<int>& nums)
+    {
+        if(left > right) return 0;
+        if(left == right)
         {
-            sum = sum < 0 ? num : sum + num;
-            ans = max(ans, sum);
+            return nums[left];
         }
         
-        return ans;
+        int mid = (right - left) / 2 + left;
+        
+        
+        int leftMax = divideConquer(left, mid, nums);
+        int rightMax = divideConquer(mid + 1, right, nums);
+        
+        int l = mid;
+        int totalLeft = 0;
+        int maxTotalLeft = INT_MIN;
+        
+        while(l >= left)
+        {
+            totalLeft += nums[l--];
+            maxTotalLeft= max(maxTotalLeft, totalLeft);
+        }
+        
+        
+        int r = mid + 1;
+        int totalRight = 0;
+        int maxTotalRight = INT_MIN;
+        
+        while(r <= right)
+        {
+            totalRight += nums[r++];
+            maxTotalRight = max(maxTotalRight, totalRight);
+        }
+        
+        // cout << left << "," << right << ", " << leftMax << " " << rightMax << " " << totalLeft + totalRight << endl;
+        return max(leftMax, max(rightMax, maxTotalLeft + maxTotalRight));
     }
 };
