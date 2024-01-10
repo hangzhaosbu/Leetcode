531cf3d837a44fc5052f1c353251fe811cd24042
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2024-01-10 14:05:13 -0500

    Time: 55 ms (60.93%), Space: 23.9 MB (64.04%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int count = 1;
+    bool validPalindrome(string s) {
+        int l = 0, r = s.length() - 1;
+        
+        
+        while(l < r)
+        {
+            if(s[l] == s[r])
+            {
+                l++;
+                r--;
+            }
+            else if(count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                count--;
+                
+                if(validPalindrome(s.substr(l + 1, r - l)) || validPalindrome(s.substr(l, r - l)))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
