dde5156f1832da2763eee73fd58c265663ffed5c
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2024-01-11 18:15:27 -0500

    Time: 74 ms (95.08%), Space: 55.6 MB (94.20%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,15 +1,54 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+        return quickselect(0, nums.size() - 1, nums, k);
+    }
+    
+    int quickselect(int left, int right, vector<int>& nums, int k)
+    {
+        // S S S S S O O O L L L L
+        if(left == right) return nums[left];
+        int r = left + rand() % (right - left);
+        int pivot = nums[r];
+        int i = left, t = left, j = right;
+        int equals = 0;
         
-        for(auto& num : nums)
+        while(t <= j)
         {
-            pq.push(num);
-            
-            if(pq.size() > k) pq.pop();
+            if(nums[t] < pivot)
+            {
+                int temp = nums[t];
+                nums[t] = nums[i];
+                nums[i] = temp;
+                
+                i++;
+                t++;
+            }
+            else if(nums[t] > pivot)
+            {
+                int temp = nums[t];
+                nums[t] = nums[j];
+                nums[j] = temp;
+                
+                j--;
+            }
+            else
+            {
+                t++;
+                equals++;
+            }
         }
         
-        return pq.top();
+        if(right - j + 1 == k) return nums[j];
+        else if(right - j + 1 > k)
+        {
+            return quickselect(j + 1, right, nums, k);
+        }
+        else
+        {
+            if(equals >= 99990) return nums[i];
+            return quickselect(left, j - 1, nums, k - (right - j + 1));
+        }
     }
 };
