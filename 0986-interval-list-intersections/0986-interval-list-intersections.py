25d9a396d3429322d011344aa9470a50441f95f0
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2024-01-20 13:07:44 -0500

    Time: 118 ms (95.06%), Space: 17.6 MB (55.24%) - LeetHub

---
+++ 0986-interval-list-intersections/0986-interval-list-intersections.py
@@ -0,0 +1,24 @@
+class Solution:
+    def intervalIntersection(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        ans = []
+        stack = []
+        
+        A = A + B
+        A.sort()
+        
+        for interval in A:
+            if not stack:
+                stack.append(interval)
+            else:
+                if interval[0] <= stack[-1][1]:
+                    ans.append([max(interval[0], stack[-1][0]), min(stack[-1][1], interval[1])])
+                    
+                    newInterval = [stack[-1][0], max(interval[1], stack[-1][1])]
+                    stack.pop()
+                    stack.append(newInterval)
+                else:
+                    stack.pop()
+                    stack.append(interval)
+        
+        return ans
+        
