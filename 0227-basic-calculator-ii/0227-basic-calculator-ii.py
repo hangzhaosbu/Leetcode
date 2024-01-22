c6b44ec2d0b3f6e3a5db450bf7b7592e2fd87dac
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2024-01-21 22:59:02 -0500

    Time: 83 ms (91.35%), Space: 19.2 MB (88.00%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,34 +1,33 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        
         curtNum = 0
+        prevNum = 0
+        ans = 0
         sign = "+"
-        
+        # "2*3+2*2"
         for i, c in enumerate(s):
             if c >= '0' and c <= '9':
                 curtNum = curtNum * 10 + ord(c) - ord('0')
             
-            
             if c in "+-*/" or i == len(s) - 1:
                 
                 if sign == "+":
-                    stack.append(curtNum)
+                    ans += prevNum
+                    prevNum = curtNum
                 elif sign == "-":
-                    stack.append(-curtNum)
+                    ans += prevNum
+                    prevNum = -curtNum
                     
                 elif sign == "*":
-                    stack[-1] = stack[-1] * curtNum
+                    prevNum = prevNum * curtNum
                 else:
-                    stack[-1] = int(stack[-1] / curtNum)
+                    prevNum = int(prevNum / curtNum)
                 
                 sign = c
+                
                 curtNum = 0
                 
-        ans = 0
-        
-        while stack:
-            ans += stack[-1]
-            stack.pop()
+        if prevNum:
+            ans += prevNum
         
         return ans
