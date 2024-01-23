8d4979c97bae85abfa1fa1f60cf02e2a418ff10a
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2024-01-23 11:00:28 -0500

    Time: 98 ms (45.65%), Space: 21.5 MB (70.65%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,8 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         hashmap = {}
-        pq = []
-        ans = []
         
         for num in nums:
             hashmap[num] = hashmap.get(num, 0) + 1
@@ -10,13 +8,36 @@ class Solution:
         candidates = []
         
         for key, val in hashmap.items():
-            heapq.heappush(pq, [val, key])
+            candidates.append([key,val])
             
-            if len(pq) > k:
-                heapq.heappop(pq)
-        
-        while pq:
-            ans.append(pq[0][1])
-            heapq.heappop(pq)
-        
-        return ans
+            
+        def quickselect(l, r, k):
+            if l == r:
+                return [candidates[l][0]]
+            
+            mid = (r - l) // 2 + l
+            pivot = candidates[mid][1]
+            
+            i, t, j = l, l, r
+            
+            while t <= j:
+                if candidates[t][1] < pivot:
+                    candidates[t], candidates[i] = candidates[i], candidates[t]
+                    i += 1
+                    t += 1
+                elif candidates[t][1] > pivot:
+                    candidates[t], candidates[j] = candidates[j], candidates[t]
+                    j -= 1
+                else:
+                    t += 1
+            
+            # S S S S E E E E L L L L
+            
+            if r - j + 1 == k:
+                return [x[0] for x in candidates[j:r + 1]]
+            elif r - j + 1 > k:
+                return quickselect(j + 1, r, k)
+            else:
+                return [x[0] for x in candidates[j:r + 1]] + quickselect(l, j - 1, k - (r - j + 1))
+            
+        return quickselect(0, len(candidates) - 1, k)
