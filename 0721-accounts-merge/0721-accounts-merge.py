5bc9d24916f9325bd63238ece0287c2c9631d7b1
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2024-01-23 10:07:12 -0500

    Time: 169 ms (91.15%), Space: 20.2 MB (99.21%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -5,7 +5,7 @@ class UnionFind:
     
     def find(self, x):
         while x != self.parent[x]:
-            self.parent[x] = self.parent[self.parent[x]]
+            self.parent[x] = self.find(self.parent[x])
             x = self.parent[x]
         return x
     
