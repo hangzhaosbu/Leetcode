1bbdbe61a3895394d26e1e22803f755e6d159bea
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2024-01-23 21:11:50 -0500

    Time: 539 ms (99.65%), Space: 22.7 MB (79.29%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,41 +1,7 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        for i in range(len(points)):
-            x, y = points[i][0], points[i][1]
-            points[i] = [x**2 + y**2, x, y]
+        points.sort(key=lambda x : x[0]*x[0] + x[1]*x[1])
         
+        # print(points)
         
-        def quickselect(l, r, points, k):
-            if l == r:
-                return [[points[l][1], points[l][2]]]
-            
-            mid = (r - l) // 2 + l
-            pivot = points[mid][0]
-            
-            i = l
-            t = l
-            j = r
-            
-            """
-            S S S E E E L L L L
-            """
-            
-            while t <= j:
-                if points[t][0] < pivot:
-                    points[t], points[i] = points[i], points[t]
-                    t += 1
-                    i += 1
-                elif points[t][0] > pivot:
-                    points[t], points[j] = points[j], points[t]
-                    j -= 1
-                else:
-                    t += 1
-                    
-            if j - l + 1 == k:
-                return [[x, y] for _, x, y in points[l:j + 1]]
-            elif j - l + 1 > k:
-                return quickselect(l, j - 1, points, k)
-            else:
-                return [[x, y] for _, x, y in points[l:j + 1]] + quickselect(j + 1, r, points, k - (j - l + 1))
-        
-        return quickselect(0, len(points) - 1, points, k)
+        return points[:k]
