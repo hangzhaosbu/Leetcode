8ca5b6d734afdecf8b463aa7fc2fee5370dcb6d8
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2024-01-23 22:01:42 -0500

    Time: 1905 ms (88.38%), Space: 24.3 MB (95.07%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -1,9 +1,8 @@
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
-        area = {}
-        color = 2
         m, n = len(grid), len(grid[0])
-        
+        color = 2
+        hashmap = {}
         ans = 0
         
         def getArea(x, y, m, n, color):
@@ -11,37 +10,35 @@ class Solution:
                 return 0
             
             grid[x][y] = color
+            
             return 1 + getArea(x + 1, y, m, n, color) + getArea(x - 1, y, m, n, color) + getArea(x, y + 1, m, n, color) + getArea(x, y - 1, m, n, color)
         
         
+        def getColor(x, y):
+            if x < 0 or x >= m or y < 0 or y >= n or grid[x][y] < 2:
+                return None
+            
+            return grid[x][y]
         
         for i in range(m):
             for j in range(n):
-                
                 if grid[i][j] == 1:
-                    
-                    area[color] = getArea(i, j, m, n, color)
+                    hashmap[color] = getArea(i, j, m, n, color)
                     color += 1
-        
+                    
         
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 0:
-                    curtArea = 1
-                    
-                    colors = set()
-                    
-                    
-                    for x, y in [[i + 1, j], [i - 1, j], [i, j + 1], [i, j - 1]]:
-                        if x < 0 or x >= m or y < 0 or y >= n or grid[x][y] == 0:
+                    curt = 1
+                    for c in set([getColor(i + 1, j), getColor(i - 1, j), getColor(i, j + 1), getColor(i, j - 1)]):
+                        if c == None:
                             continue
-                        colors.add(grid[x][y])
+                        
+                        curt += hashmap[c]
                     
-                    
-                    for color in colors:
-                        curtArea += area[color]
-                    
-                    ans = max(ans, curtArea)
+                    ans = max(ans, curt)
         
-        return ans if ans != 0 else sum(area.values())
+        return ans if ans != 0 else hashmap[2]
+                        
                     
