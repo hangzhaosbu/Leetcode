29dedf62320f7d069b88318ad9f6745396b005cc
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2024-01-24 23:50:36 -0500

    Time: 3343 ms (5.00%), Space: 27.5 MB (5.10%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,16 +1,33 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        pq = []
-        for point in points:
-            
-            heapq.heappush(pq, [-point[0]*point[0]-point[1]*point[1],point[0],point[1]])
-            
-            if len(pq) > k:
-                heapq.heappop(pq)
+        for i in range(len(points)):
+            points[i] = [points[i][0]*points[i][0]+points[i][1]*points[i][1],points[i][0],points[i][1]]
         
-        ans = []
-        while pq:
-            ans.append([pq[0][1], pq[0][2]])
-            heapq.heappop(pq)
+        def quickselect(l, r, k):
+            if l == r:
+                return [[points[l][1], points[l][2]]]
+            
+            mid = (r - l) // 2 + l
+            pivot = points[mid][0]
+            
+            i, t, j = l, l, r
+            
+            while t <= j:
+                if points[t][0] < pivot:
+                    points[i], points[t] = points[t], points[i]
+                    i += 1
+                    t += 1
+                elif points[t][0] > pivot:
+                    points[j], points[t] = points[t], points[j]
+                    j -= 1
+                else:
+                    t += 1
+            
+            if i - l + 1 == k:
+                return [[x[1], x[2]] for x in points[l : i + 1]]
+            elif i - l + 1 > k:
+                return quickselect(l, i - 1, k)
+            else:
+                return [[x[1], x[2]] for x in points[l : i + 1]] + quickselect(i + 1, r, k - (i - l + 1))
         
-        return ans
+        return quickselect(0, len(points) - 1, k)
