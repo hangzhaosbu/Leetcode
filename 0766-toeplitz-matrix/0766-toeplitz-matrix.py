6741739667a0716db72795ed0be5d454990c80a9
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2024-01-25 18:11:00 -0500

    Time: 83 ms (53.99%), Space: 16.9 MB (52.13%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,8 +1,17 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        for i in range(len(matrix)):
+        buffer = []
+        
+        for j in range(len(matrix[0])):
+            buffer.append(matrix[0][j])
+            
+        
+        for i in range(1, len(matrix)):
             for j in range(len(matrix[0])):
-                if i > 0 and j > 0 and matrix[i][j] != matrix[i - 1][j - 1]:
+                if j > 0 and matrix[i][j] != buffer[j - 1]:
                     return False
+            
+            buffer.pop()
+            buffer.insert(0, matrix[i][0])
         
         return True
