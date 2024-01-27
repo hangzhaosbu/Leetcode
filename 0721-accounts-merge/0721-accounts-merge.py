bbd89351619edfc2ceef0204fea741ad5dad97f4
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2024-01-27 10:16:41 -0500

    Time: 171 ms (87.17%), Space: 20.2 MB (97.46%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,12 +1,14 @@
 class UnionFind:
+    
     def __init__(self, n):
         self.parent = [i for i in range(n)]
-        self.rank = [1] * n
+        self.rank = [1 for _ in range(n)]
     
     def find(self, x):
         while x != self.parent[x]:
-            self.parent[x] = self.find(self.parent[x])
+            self.parent[x] = self.parent[self.parent[x]]
             x = self.parent[x]
+        
         return x
     
     def union(self, x, y):
@@ -14,67 +16,68 @@ class UnionFind:
         
         if px == py:
             return False
-        
-        if self.rank[px] > self.rank[py]:
-            self.parent[py] = px
-            self.rank[px] += self.rank[py]
         else:
-            self.parent[px] = py
-            self.rank[py] += self.rank[px]
+            
+            if self.rank[px] > self.rank[py]:
+                self.parent[py] = px
+                self.rank[px] += self.rank[py]
+            else:
+                self.parent[px] = py
+                self.rank[py] += self.rank[px]
+            
+            return True
         
-        return True
-
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         uf = UnionFind(len(accounts))
+        
         hashmap = {}
-        candidates = {}
+        groups = {}
         ans = []
         
-        for i, acc in enumerate(accounts):
-            email = acc[1:]
+        for i, account in enumerate(accounts):
+            emails = account[1:]
             
-            for e in email:
-                if e not in hashmap:
-                    hashmap[e] = i
+            for email in emails:
+                if email not in hashmap:
+                    hashmap[email] = i
                 else:
-                    uf.union(i, hashmap[e])
+                    uf.union(i, hashmap[email])
+                    
         
         for key, val in hashmap.items():
             leader = uf.find(val)
             
-            if leader not in candidates:
-                candidates[leader] = []
+            if leader not in groups:
+                groups[leader] = []
             
-            candidates[leader].append(key)
+            groups[leader].append(key)
         
-        for key, val in candidates.items():
-            
-            user = [accounts[key][0]]
-            emails = sorted(val)
-            ans.append(user + emails)
+        
+        for key, val in groups.items():
+            ans.append([accounts[key][0]] + sorted(val))
+        
         
         return ans
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
