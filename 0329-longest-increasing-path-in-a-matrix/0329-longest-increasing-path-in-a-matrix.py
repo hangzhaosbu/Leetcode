1fd8d68e5511a85cc4020e2de6a215a0b9bc1994
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2024-01-27 22:46:21 -0500

    Time: 298 ms (82.31%), Space: 17.5 MB (89.53%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,35 +1,35 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        
         m, n = len(matrix), len(matrix[0])
-        directions = [[1,0],[-1,0],[0,1],[0,-1]]
-        dp = [[0 for _ in range(n)] for _ in range(m)]
+        
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
         ans = 0
         
-        def dfs(x, y, m, n):
-            if dp[x][y] != 0:
-                return dp[x][y]
+        def dfs(i, j):
+            if dp[i][j] != -1:
+                return dp[i][j]
             
-            steps = 0
+            used = 0
             
-            for direction in directions:
-                newx = x + direction[0]
-                newy = y + direction[1]
+            for direction in [[1,0],[-1,0],[0,1],[0,-1]]:
+                newx, newy = i + direction[0], j + direction[1]
                 
-                if newx < 0 or newx >= m or newy < 0 or newy >= n:
+                if newx < 0 or newx >= len(matrix) or newy < 0 or newy >= len(matrix[0]):
+                    continue
+                
+                if matrix[newx][newy] <= matrix[i][j]:
                     continue
                     
-                if matrix[newx][newy] <= matrix[x][y]:
-                    continue
-                
-                steps = max(steps, dfs(newx, newy, m, n))
+                used = max(used, dfs(newx, newy))
             
-            dp[x][y] = steps + 1
-            return dp[x][y]
+            dp[i][j] = used + 1
+            return dp[i][j]
         
         for i in range(m):
             for j in range(n):
-                ans = max(ans, dfs(i, j, m, n))
-                
+                ans = max(ans, dfs(i, j))
         
         return ans
+        
+                
+                
