2166fe18493e98f3a59124e677f285022da82971
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2024-07-29 23:32:48 -0700

    Time: 38 ms (88.77%), Space: 18.1 MB (17.96%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,20 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
+        s = re.sub(r'[\W_]+', '', s)
         
         l, r = 0, len(s) - 1
         
         while l < r:
-            while l < r and s[l] not in "0123456789abcdefghijklmnopqrstuvwxyz":
-                l+=1
-            
-            while l < r and s[r] not in "0123456789abcdefghijklmnopqrstuvwxyz":
-                r-=1
-            
-            if s[l] == s[r]:
-                l+=1
-                r-=1
-            else:
+            if s[l]!=s[r]:
                 return False
+            l+=1
+            r-=1
         
         return True
+        
+        
