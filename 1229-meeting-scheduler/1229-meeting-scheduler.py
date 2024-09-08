cef8d4c039318ca403642a04b0f73b22b0171b11
Hang Zhao <37938674+hangzhaosbu@users.noreply.github.com>
2024-09-08 17:45:22 -0400

    Time: 482 ms (14.45%), Space: 24.2 MB (77.49%) - LeetHub

---
+++ 1229-meeting-scheduler/1229-meeting-scheduler.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
+
+        slots1.sort()
+        slots2.sort()
+        pointer1 = pointer2 = 0
+
+        while pointer1 < len(slots1) and pointer2 < len(slots2):
+            # find the boundaries of the intersection, or the common slot
+            intersect_right = min(slots1[pointer1][1], slots2[pointer2][1])
+            intersect_left = max(slots1[pointer1][0],slots2[pointer2][0])
+            if intersect_right - intersect_left >= duration:
+                return [intersect_left, intersect_left + duration]
+            # always move the one that ends earlier
+            if slots1[pointer1][1]< slots2[pointer2][1]:
+                pointer1 += 1
+            else:
+                pointer2 += 1
+        return []
